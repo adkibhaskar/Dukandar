4e8fdf91cbee14cb75aa5409866a90517a9cd95e
adkibhaskar <adkibhaskar2002@gmail.com>
2024-07-07 18:40:43 +0530

    Add User Model

---
+++ server/models/User.js
@@ -0,0 +1,53 @@
+const mongoose=require("mongoose");
+
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    shopName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    shopAddress:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    contactNumber:{
+        type:Number,
+        required:true,
+        trim:true
+    },
+    grocery:[
+        {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Grocery"
+    }
+],
+   accountType:{
+    type:String,
+    enum:["Customer","WholeSaler","Admin"],
+    required:true
+   }
+},
+{timestamps:true}
+);
+
+module.exports=mongoose.model("User",userSchema)
